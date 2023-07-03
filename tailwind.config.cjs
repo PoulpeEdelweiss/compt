8f4f023613cb317fbe14499d3abc6cb247e69a6a
PoulpeEdelweiss <110398634+PoulpeEdelweiss@users.noreply.github.com>
2023-07-04 02:55:57 +0300

    подключение шрифта montserrat

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,19 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		screens: {
+		sm: '640px',
+		md:'768px',
+		lg:'1024px',
+		xl: '1280px',
+		},
+		extend: {
+			fontFamily: {
+				body: ['Montserrat', ...defaultTheme.fontFamily.sans],
+				
+			},
+
+		},
 	},
 	plugins: [],
 }
