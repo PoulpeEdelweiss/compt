55621eb092e9fdcedd9b415e44ee952ff5b73aa1
PoulpeEdelweiss <110398634+PoulpeEdelweiss@users.noreply.github.com>
2023-10-02 00:15:38 +0300

    тестовая страница с счётчиком

---
+++ src/components/Header__test/Header__test.vue
@@ -0,0 +1,19 @@
+<template>
+    <button
+        @click="increment"  
+    >
+        add
+    </button>
+    <div>
+        <p>bunny dust</p>
+        <span>{{ counter }}</span>
+    </div>
+</template>
+<script setup>
+    import { ref } from 'vue'
+    const counter = ref(0)
+    function increment() {
+      counter.value++
+    }
+    console.log("counter.value")
+</script>
